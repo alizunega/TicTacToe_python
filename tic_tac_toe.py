8535dd72959c3a2b09553743f5429f6ad56d0988
alizunega <alizunega84@gmail.com>
2024-05-06 18:27:26 -0300

    progreso

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,49 +1,82 @@
-from tablero import *
+from random import randrange
 
-##lista - tuplas de posibles wins
-wins_tuples = [
-    ((0, 0), (1, 0), (2, 0)),
-    ((0, 0), (0, 1), (0, 2)),
-    ((0, 0), (1, 1), (2, 2)),
-    ((1, 0), (1, 1), (1, 2)),
-    ((2, 0), (2, 1), (2, 2)),
-    ((0, 1), (1, 1), (2, 1)),
-    ((0, 2), (1, 2), (2, 2)),
-    ((0, 2), (1, 1), (2, 0)),
-]
-
-# tableero
+# valor original de board
 board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-
-# diccionario de coordenadas - valor
-coord_valor = {
-    (0, 0): board[0][0],
-    (0, 1): board[0][1],
-    (0, 2): board[0][2],
-    (1, 0): board[1][0],
-    (1, 1): board[1][1],
-    (1, 2): board[1][2],
-    (2, 0): board[2][0],
-    (2, 1): board[2][1],
-    (2, 2): board[2][2],
-}
+win = False
 
 
-# lista de casilleros tomados
-# contiene el 5 porque por defecto la maquina lo ocupa
-lista_ocupados = [5]
-board[1][1] = "X"
+def display_board(board):
+    # La función acepta un parámetro el cual contiene el estado actual del tablero
+    # y lo muestra en la consola.
+    b_board = board
+
+    for i in range(3):
+        print("+-------+-------+-------+")
+        print("|       |       |       |")
+        for j in range(3):
+            print(f"|   {b_board[i][j]}   ", end="")
+        print("|")
+        print("|       |       |       |")
+    print("+-------+-------+-------+")
 
 
-# solicitud ingreso de valor y comprobacion
-user_choice = int(input("Ingresa tu movimiento: "))
+def enter_move(board):
+    # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,
+    # verifica la entrada y actualiza el tablero acorde a la decisión del usuario.
 
-while user_choice in range(0, 10):
-    if user_choice not in lista_ocupados:
-        # asigno valor a lista
-        lista_ocupados.append(user_choice)
-        # modifico el diccionario para que ahora pueda modificar el tablero
-        for key, value in coord_valor.items():
-            if value == user_choice:
-                coord_valor[key] = "O"
-            break
+    user_choice = int(input("Ingresa tu movimiento: "))
+
+    if user_choice in range(1, 10):
+        if user_choice in board:
+            # asigno valor a lista
+            print(user_choice)
+
+
+def make_list_of_free_fields(board):
+    # La función examina el tablero y construye una lista de todos los cuadros vacíos.
+    # La lista esta compuesta por tuplas, cada tupla es un par de números que indican la fila y columna.
+    free_fields = []
+    for i in range(0, 3):
+        for j in range(0, 3):
+            if isinstance(board[i][j], int):
+                free_fields.append((i, j))
+
+    print(free_fields)
+    return free_fields
+
+
+def victory_for(board, sign):
+    # La función analiza el estatus del tablero para verificar si
+    # el jugador que utiliza las 'O's o las 'X's ha ganado el juego.
+    wins_tuples = [
+        ((0, 0), (1, 0), (2, 0)),
+        ((0, 0), (0, 1), (0, 2)),
+        ((0, 0), (1, 1), (2, 2)),
+        ((1, 0), (1, 1), (1, 2)),
+        ((2, 0), (2, 1), (2, 2)),
+        ((0, 1), (1, 1), (2, 1)),
+        ((0, 2), (1, 2), (2, 2)),
+        ((0, 2), (1, 1), (2, 0)),
+    ]
+
+    print("algo")
+
+
+def draw_move(board):
+    # La función dibuja el movimiento de la máquina y actualiza el tablero.
+
+    print("algo")
+
+
+print("TIC-TAC_TOE")
+display_board(board)
+print("Deseas comenzar el juego??")
+comienzo = input("Ingresa y para comenzar, o n para finalizar: ")
+if comienzo.lower() == "y":
+    board[1][1] = "X"
+    free_fields = make_list_of_free_fields(board)
+    if free_fields == []:
+        print("El juego terminó, no hay movimientos disponibles")
+    else:
+        display_board(board)
+        enter_move(board)
