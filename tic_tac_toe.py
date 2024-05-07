87a0d6a0b4c6b90c042114039b1c86984709bc48
alizunega <alizunega84@gmail.com>
2024-05-07 11:35:16 -0300

    increible pero no funciona

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -20,32 +20,40 @@ def display_board(board):
     print("+-------+-------+-------+")
 
 
-####################################################
+######################### Movimiento del usuario ###########################
 def enter_move(board):
     # La función acepta el estado actual del tablero y pregunta al usuario acerca de su movimiento,
     # verifica la entrada y actualiza el tablero acorde a la decisión del usuario.
     lista_libres = make_list_of_free_fields(board)
-    user_choice = int(input("Ingresa tu movimiento: "))
-    print(user_choice)
+    user_choice = input("Ingresa tu movimiento: ")
+    if user_choice.isdigit():
+        user_choice = int(user_choice)
 
-    if user_choice in range(1, 10):
-        tomado = False
-        for i in range(0, 3):
-            for j in range(0, 3):
-                if (i, j) in lista_libres and board[i][j] == user_choice:
-                    board[i][j] = "O"
-                    tomado = True
-        if not tomado:
-            print(f"Ingrese un nuevo valor,{user_choice} este ya fue tomado")
+        if user_choice in range(1, 10):
+            tomado = False
+            for i in range(0, 3):
+                for j in range(0, 3):
+                    if (i, j) in lista_libres and board[i][j] == user_choice:
+                        board[i][j] = "O"
+                        print("Usuario Eligió: ", user_choice)
+                        tomado = True
+            if not tomado:
+                print(f"Ingrese un nuevo valor,{user_choice} este ya fue tomado")
+                enter_move(board)
+
+        else:
+            print("El movimiento debe ser un valor entre 1 y 9")
+            enter_move(board)
 
     else:
         print("El movimiento debe ser un valor entre 1 y 9")
         print("Chequee el tablero para ver los casilleros ya asignados")
+        enter_move(board)
 
     display_board(board)
 
 
-####################################################
+######################### Lista de coordenadas libres ###########################
 def make_list_of_free_fields(board):
     # La función examina el tablero y construye una lista de todos los cuadros vacíos.
     # La lista esta compuesta por tuplas, cada tupla es un par de números que indican la fila y columna.
@@ -58,7 +66,9 @@ def make_list_of_free_fields(board):
     return free_fields
 
 
-####################################################
+####################### Comprueba si alguien ganó #############################
+
+
 def victory_for(board, sign):
     win = False
     # La función analiza el estatus del tablero para verificar si
@@ -80,14 +90,29 @@ def victory_for(board, sign):
             if board[i][j] == sign:
                 sign_list.append((i, j))
 
-    for lista in wins_tuples:
-        if wins_tuples[lista] in sign_list:
-            win = True
+    if len(sign_list) < 3:
+        return win
+    else:
+        for lista in wins_tuples:
+            if sign_list in lista:
+                win = True
+
+    # if len(sign_list) < 3:
+    #     return win
+    # else:
+    #     for lista in wins_tuples:
+    #         for i in range(len(sign_list) - 1):
+    #             ganadores = 0
+    #             if sign_list[i] in lista:
+    #                 ganadores += 1
+
+    #     if ganadores >= 1:
+    #         win = True
 
     return win
 
 
-####################################################
+######################## Movimiento de la máquina ############################
 def draw_move(board):
     # La función dibuja el movimiento de la máquina y actualiza el tablero.
     lista_libres = make_list_of_free_fields(board)
@@ -95,6 +120,7 @@ def draw_move(board):
     if lista_libres:  # Verificar si hay campos libres disponibles
         # Elegir aleatoriamente una casilla de la lista de campos libres
         i, j = choice(lista_libres)
+        print("Maquina Eligió: ", board[i][j])
         board[i][j] = "X"
     else:
         print("No quedan movimientos por hacer")
@@ -107,25 +133,27 @@ def draw_move(board):
 print("TIC-TAC_TOE")
 display_board(board)
 print("Deseas comenzar el juego??")
-comienzo = input("Ingresa y para comenzar, o n para finalizar: ")
+comienzo = input("Ingresa y para comenzar u otra tecla para finalizar: ")
 if comienzo.lower() == "y":
     board[1][1] = "X"
     free = []
-    count = 1
     display_board(board)
-    while count <= 5:  # controla que se haya hecho un minimo de movimientos
-        enter_move(board)  # movimiento de usuario y actualiza
-        draw_move(board)  # movimiento de maquina y actualiza
-        count += 2
 
     free = make_list_of_free_fields(board)
-    while not victory_for(board, "O") and free != []:
+
+    while free and not victory_for(board, "X") and not victory_for(board, "O"):
         enter_move(board)
         draw_move(board)
-        result = victory_for(board, "X")
         free = make_list_of_free_fields(board)
 
-    if not result:
+    if victory_for(board, "X"):
+        print("Has perdido!")
+
+    elif victory_for(board, "O"):
+        print("Has ganado!")
+
+    if make_list_of_free_fields(board) == []:
         print("No quedan movimientos por hacer")
+        print("El juego quedó empatado!")
 else:
     print("Lamento que no quieras jugar")
